c59ec700d13be183406548ce50f3aa1aab85ce68
Hyunseung Yoo <hsyoo@anl.gov>
2018-04-16 10:28:36 -0500

    cleanup obsolete code

--- Pilot1/Combo/candle_infer.py
+++ Pilot1/Combo/candle_infer.py
@@ -52,43 +52,6 @@ class PermanentDropout(keras.layers.Dropout):
         return x
 
 
-def get_parser(description=None):
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-s', '--sample_set',
-                        default='NCIPDM',
-                        help='cell sample set: NCI60, NCIPDM, GDSC, ...')
-    parser.add_argument('-d', '--drug_set',
-                        default='ALMANAC',
-                        help='drug set: ALMANAC, GDSC, NCI_IOA_AOA, ...')
-    parser.add_argument('-z', '--batch_size', type=int,
-                        default=100000,
-                        help='batch size')
-    parser.add_argument('--step', type=int,
-                        default=10000,
-                        help='number of rows to inter in each step')
-    parser.add_argument('-m', '--model_file',
-                        default='saved.model.h5',
-                        help='trained model file')
-    parser.add_argument('-n', '--n_pred', type=int,
-                        default=1,
-                        help='the number of predictions to make for each sample-drug combination for uncertainty quantification')
-    parser.add_argument('-w', '--weights_file',
-                        default='saved.weights.h5',
-                        help='trained weights file (loading model file alone sometimes does not work in keras)')
-    parser.add_argument('--ns', type=int,
-                        default=0,
-                        help='the first n entries of cell samples to subsample')
-    parser.add_argument('--nd', type=int,
-                        default=0,
-                        help='the first n entries of drugs to subsample')
-    parser.add_argument("--si", type=int,
-                         default=0,
-                         help='the index of the first cell sample to subsample')
-    parser.add_argument("--use_landmark_genes", action="store_true",
-                        help="use the 978 landmark genes from LINCS (L1000) as expression features")
-
-    return parser
-
 
 def lookup(df, sample, drug1, drug2=None, value=None):
     drug2 = drug2 or drug1
@@ -136,12 +99,6 @@ def prepare_data(sample_set='NCI60', drug_set='ALMANAC', use_landmark_genes=Fals
     # df_desc = NCI60.load_drug_descriptors_new()
     df_desc = NCI60.load_drug_set_descriptors(drug_set=drug_set)
     return df_expr, df_desc
-
-#def initialize_parameters():
-#    description = 'Infer drug pair response from trained combo model.'
-#    parser = get_parser(description)
-#    args = parser.parse_args()
-#    return args
 
 def initialize_parameters():
     from default_utils import str2bool
