81a274f45ceccd529026942762ad5de6d689d347
carrondt <carron@anl.gov>
2018-10-02 09:24:57 -0500

    adds 'no_gpu_prof' as option for theta and also a small graphing script

---
+++ common/graph_ut.py
@@ -0,0 +1,20 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+path = '/Users/dcarron/Documents/cpu_sample.txt'
+
+with open(path) as file:
+    lines = file.readlines()
+
+print(lines)
+
+tmp = []
+for x in lines:
+    split = x.split('|')
+    print(float(split[1]))
+    tmp.append(float(split[1]))
+
+plt.plot(tmp)
+x1,x2,y1,y2 = plt.axis()
+plt.axis((x1,x2,0,100))
+plt.show()
