f5a569c189dd3cc5baed2fc5a958601a2450ba9e
Hyunseung Yoo <hsyoo@anl.gov>
2019-03-15 21:56:38 -0500

    include y_ metadata in right order

--- Pilot1/Uno/uno_baseline_keras2.py
+++ Pilot1/Uno/uno_baseline_keras2.py
@@ -352,8 +352,8 @@ def run(params):
                 for j, input_feature in enumerate(x_list):
                     input_feature.columns = [''] * len(input_feature.columns)
                     store.append('x_{}_{}'.format(partition, j), input_feature.astype('float32'), format='table', data_column=True)
-                y[target] = y[target].astype('float32')
-                store.append('y_{}'.format(partition), y, format='table', data_column=True)
+                store.append('y_{}'.format(partition), y.astype({target:'float32'}), format='table', data_column=True,
+                        min_itemsize={'Sample':30, 'Drug1':10, 'Drug2':10})
                 logger.info('Generating {} dataset. {} / {}'.format(partition, i, gen.steps))
         store.close()
         logger.info('Completed generating {}'.format(fname))
@@ -444,7 +444,7 @@ def run(params):
             train_gen = CombinedDataGenerator(loader, fold=fold, batch_size=args.batch_size, shuffle=args.shuffle)
             val_gen = CombinedDataGenerator(loader, partition='val', fold=fold, batch_size=args.batch_size, shuffle=args.shuffle)
 
-        df_val = val_gen.get_response()
+        df_val = val_gen.get_response(copy=True)
         y_val = df_val[target].values
         y_shuf = np.random.permutation(y_val)
         log_evaluation(evaluate_prediction(y_val, y_shuf),
@@ -500,8 +500,7 @@ def run(params):
     if args.agg_dose:
         df_pred.sort_values(['Source', 'Sample', 'Drug1', 'Drug2', target], inplace=True)
     else:
-        # df_pred.sort_values(['Source', 'Sample', 'Drug1', 'Drug2', 'Dose1', 'Dose2', 'Growth'], inplace=True)
-        df_pred.sort_values(['Sample', 'Drug1', 'Drug2', 'Dose1', 'Dose2', 'Growth'], inplace=True)
+        df_pred.sort_values(['Source', 'Sample', 'Drug1', 'Drug2', 'Dose1', 'Dose2', 'Growth'], inplace=True)
     df_pred.to_csv(pred_fname, sep='\t', index=False, float_format='%.4g')
 
     if args.cv > 1:
