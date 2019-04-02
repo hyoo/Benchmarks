1d974cb3c57dc1579aca2417f9d058e7c0da28e1
Hyunseung Yoo <hsyoo@anl.gov>
2019-04-02 14:11:05 -0500

    cv infer runs with --n_pred

--- Pilot1/Uno/uno_infer.py
+++ Pilot1/Uno/uno_infer.py
@@ -3,11 +3,14 @@
 import argparse
 import logging
 
-import keras
 import pandas as pd
+import numpy as np
+import keras
 
 from uno_data import DataFeeder
-from uno_baseline_keras2 import evaluate_prediction, log_evaluation
+from uno_baseline_keras2 import evaluate_prediction
+import candle_keras
+
 
 def log_evaluation(metric_outputs, description='Comparing y_true and y_pred:'):
     print(description)
@@ -21,6 +24,9 @@ def get_parser():
                        help="data file to infer on. expect exported file from uno_baseline_keras2.py")
     parser.add_argument("--model_file", help="json model description file")
     parser.add_argument("--weights_file", help="model weights file")
+    parser.add_argument("--partition", default='all',
+                        choices=['train', 'val', 'all'], help="partition of test dataset")
+    parser.add_argument("-n", "--n_pred", type=int, default=1, help="the number of predictions to make")
 
     return parser
 
@@ -29,11 +35,10 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    test_gen = DataFeeder(filename=args.data, batch_size=1024)
-
-    if 'json' == args.model_file.split('.')[-1]:
-        with open(args.model_file, 'r') as f:
-            model_json = f.read()
+    candle_keras.register_permanent_dropout()
+    if args.model_file.split('.')[-1] == 'json':
+        with open(args.model_file, 'r') as model_file:
+            model_json = model_file.read()
             model = keras.models.model_from_json(model_json)
             model.load_weights(args.weights_file, by_name=True)
     else:
@@ -45,7 +50,6 @@ def main():
     cv_pred_list = []
     cv_y_list = []
     df_pred_list = []
-    cv_stats = {'mae': [], 'mse': [], 'r2': [], 'corr': []}
     for cv in range(args.n_pred):
         cv_pred = []
         dataset = ['train', 'val'] if args.partition == 'all' else [args.partition]
@@ -57,19 +61,14 @@ def main():
             df_y = test_gen.get_response(copy=True)
             y_test = df_y['Growth'].values
 
-            df_pred = df_y.assign(PredictedGrowth=y_test_pred, GrowthError=y_test_pred - y_test)
+            df_pred = df_y.assign(PredictedGrowth=y_test_pred, GrowthError=y_test_pred-y_test)
             df_pred_list.append(df_pred)
             test_gen.close()
 
             if cv == 0:
-                cv_y_list.append(df_y)
+                cv_y_list.append(df_pred['Growth'])
             cv_pred.append(y_test_pred)
         cv_pred_list.append(np.concatenate(cv_pred))
-
-        # calcuate stats for mse, mae, r2, corr
-        scores = evaluate_prediction(df_pred['Growth'], df_pred['PredictedGrowth'])
-        # log_evaluation(scores, description=cv)
-        [cv_stats[key].append(scores[key]) for key in scores.keys()]
 
     df_pred = pd.concat(df_pred_list)
     cv_y = pd.concat(cv_y_list)
@@ -77,7 +76,19 @@ def main():
     # save to tsv
     df_pred = df_y.assign(PredictedGrowth=y_test_pred, GrowthError=y_test_pred-y_test)
     df_pred.sort_values(['Sample', 'Drug1', 'Drug2', 'Dose1', 'Dose2', 'Growth'], inplace=True)
-    df_pred.to_csv('prediction.tsv', sep='\t', index=False, float_format='%.4g')
+    df_pred.to_csv('uno_pred.all.tsv', sep='\t', index=False, float_format='%.4g')
+
+    df_sum = pd.DataFrame()
+    df_sum['Growth'] = cv_y
+    df_sum['PredGrowthMean'] = np.mean(cv_pred_list, axis=0)
+    df_sum['PredGrowthStd'] = np.std(cv_pred_list, axis=0)
+    df_sum['PredGrowthMin'] = np.min(cv_pred_list, axis=0)
+    df_sum['PredGrowthMax'] = np.max(cv_pred_list, axis=0)
+
+    df_sum.to_csv('uno_pred.tsv', index=False, sep='\t', float_format='%.4f')
+
+    scores = evaluate_prediction(df_sum['Growth'], df_sum['PredGrowthMean'])
+    log_evaluation(scores, description='Testing on data from {} on partition {}, ({})'.format(args.data, args.partition, len(cv_y)))
 
 
 if __name__ == '__main__':
